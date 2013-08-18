121289f691489f4bd7cf95b44225de30e975874b
MATSUOKA Kohei <kohei@machu.jp>
2013-08-19 00:24:41 +0900

    add apache2 recipe

--- site-cookbooks/apache/recipes/default.rb
+++ site-cookbooks/apache/recipes/default.rb
@@ -6,3 +6,17 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+package "apache2" do
+	action :install
+end
+
+service "apache2" do
+	supports :status => true, :restart => true, :reload => true
+	action [:enable, :start]
+end
+
+template "/etc/apache2/sites-available/default" do
+	owner "root"
+	group "root"
+	mode  0644
+end
